b6952a3e2da35c2555d44629d5752ca33c2a1af7
nbeverton <nbeverton@gmail.com>
2023-05-26 19:02:58 -0300

    Classe Usuario
    Resolver questões do persistence.

---
+++ src/teste/basico/NovoUsuario.java
@@ -0,0 +1,10 @@
+package teste.basico;
+
+import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
+
+public class NovoUsuario {
+
+    public static void main(String[] args) {
+        EntityManagerFactoryBuilder emf = Persistence;
+    }
+}
