54d60dcd8439dc69d8926197b150796d71c64a9e
nbeverton <nbeverton@gmail.com>
2023-05-30 18:57:05 -0300

    Classe ObterUsuarios
    Construção da classe ObterUsuarios que faz uma consulta e mostro como resultado o número de usuários desejados.

--- src/teste/basico/ObterUsuario.java
+++ src/teste/basico/ObterUsuario.java
@@ -6,20 +6,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class NovoUsuario {
+public class ObterUsuario {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
         EntityManager em = emf.createEntityManager();
 
-        Usuario novoUsuario = new Usuario("Leonardo", "leo@lnc.com.br");
-
-        em.getTransaction().begin();
-        em.persist(novoUsuario);
-        em.getTransaction().commit();
-
-        System.out.println("I Id gerado foi: " + novoUsuario.getId());
+        Usuario usuario = em.find(Usuario.class, 7L);
+        System.out.println(usuario.getNome());
 
         em.close();
         emf.close();
