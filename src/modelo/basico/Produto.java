992144d325fb2471908ed113d129ca8763bab63c
nbeverton <nbeverton@gmail.com>
2023-06-02 17:54:50 -0300

    Config Annotation
    Configuração de annotation na Classe Produtos.

--- src/modelo/basico/Produto.java
+++ src/modelo/basico/Produto.java
@@ -1,17 +1,17 @@
 package modelo.basico;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "produtos")
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "prod_nome", length = 200, nullable = false)
     private String nome;
+    @Column(name = "prod_preco", nullable = false, precision = 11, scale = 2)
     private Double preco;
 
     public Produto(){
