24deabe4b95920cae396082ac259c9889f177a84
nbeverton <nbeverton@gmail.com>
2023-07-05 20:18:59 -0300

    Config na classe Assento e Cliente
    Configuração e inserção de clientes e assentos em uma relação de 1 para 1. Anotação @JoinColumn com unique setado para true (impede duplicaçao no ID).

--- src/modelo/umpraum/Cliente.java
+++ src/modelo/umpraum/Cliente.java
@@ -12,4 +12,39 @@ public class Cliente {
 
     private String nome;
 
+    @OneToOne
+    @JoinColumn(name = "assento_id", unique = true)
+    private Assento assento;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, Assento assento) {
+        this.nome = nome;
+        this.assento = assento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Assento getAssento() {
+        return assento;
+    }
+
+    public void setAssento(Assento assento) {
+        this.assento = assento;
+    }
 }
