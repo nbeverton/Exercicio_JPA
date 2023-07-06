5930bea4ff8363fac55512d2be55ab17fcbc7b4a
nbeverton <nbeverton@gmail.com>
2023-07-05 21:30:33 -0300

    Consulta bidirecional
    Bidirecionamento do relacionamento OneToOne. Fazer consulta de assento e cliente.

---
+++ src/teste/umpraum/ObterClienteAssento.java
@@ -0,0 +1,18 @@
+package teste.umpraum;
+
+import infra.DAO;
+import modelo.umpraum.Cliente;
+
+public class ObterClienteAssento {
+
+    public static void main(String[] args) {
+
+        DAO<Cliente> dao = new DAO<>(Cliente.class);
+
+        Cliente cliente = dao.obterPorID(1L);
+        System.out.println(cliente.getAssento().getNome());
+        System.out.println(cliente.getAssento().getCliente().getNome());
+
+        dao.fechar();
+    }
+}
